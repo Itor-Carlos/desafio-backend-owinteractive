da17984b5e6893155d9ab88c1d53a5b8c4398d88
Itor Carlos <itor51carlos@gmail.com>
2024-11-19 17:09:20 -0300

    add Transaction model

---
+++ database/migrations/2024_11_18_213337_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer("user_id");
+            $table->float("value",$total = 8, $places = 2);
+            $table->string("description", 64);
+            $table->enum("type", ["debit", "credit", "refund"]);
+            $table->enum("currency", ["USD", "BRL"]);
+            $table->string("notes", 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
